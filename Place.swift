710b83069c70a7ea90c09329175c8d176980dc56
jamiebarbosa <jamie.barbosa@utexas.edu>
2016-03-09 11:14:17 -0600

    business data is in a map, now need to display and implement sorting

--- Place.swift
+++ Place.swift
@@ -12,23 +12,31 @@ import UIKit
 class Place{
     var name : String
     var description: String
-    var lgbtq: Bool
-    var bathroom: Bool
-    var open_carry: Bool
-    var wheelchair: Bool
-    var vegan: Bool
-    var quiet: Bool
+    
+    var categories: [ String : Bool ]
+    
+    
+//    var lgbtq: Bool
+//    var bathroom: Bool
+//    var open_carry: Bool
+//    var wheelchair: Bool
+//    var vegan: Bool
+//    var quiet: Bool
 
 
-    init( name: String, description: String, lgbtq: Bool, bathroom: Bool, open_carry: Bool, wheelchair: Bool, vegan: Bool, quiet: Bool)
+    init( name: String, description: String, categories: Dictionary<String, Bool>)
+//    init( name: String, description: String, lgbtq: Bool, bathroom: Bool, open_carry: Bool, wheelchair: Bool, vegan: Bool, quiet: Bool)
 {
     self.name = name
     self.description = description
-    self.lgbtq = lgbtq
-    self.bathroom = bathroom
-    self.open_carry = open_carry
-    self.wheelchair = wheelchair
-    self .vegan = vegan
-    self.quiet = quiet
+    
+    self.categories = categories
+    
+//    self.lgbtq = lgbtq
+//    self.bathroom = bathroom
+//    self.open_carry = open_carry
+//    self.wheelchair = wheelchair
+//    self .vegan = vegan
+//    self.quiet = quiet
 }
 }
